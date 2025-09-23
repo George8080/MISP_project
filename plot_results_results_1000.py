8f8432fb836e7f2d6629fc95d0fb72370fe9ec79
George8080 <santisjorge8000@gmail.com>
2025-09-23 19:05:58 -0300

    Ploting results

---
+++ plot_results_results_1000.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('results_MISP_avg.csv')
+data = data[data.Size == 1000]
+data.columns = data.columns.str.strip()
+
+# print(data)
+
+plt.figure(figsize=(10, 8))
+plt.plot(data['Density'], data['Greedy_Value'], color='r', label='Valores medio usando Greedy')
+plt.plot(data['Density'], data['GreedyRan_Value'], color='b', label='Valores medio usando RandomGreedy')
+plt.title('Resultados de valores entre MISP_Greedy y MISP_RandomGreedy (Tamaño 1000)')
+plt.xlabel('Densidad')
+plt.ylabel('Tiempo (ns)')
+plt.legend()
+plt.tick_params(axis='x')
+plt.tick_params(axis='y')
+plt.grid(True)
+plt.savefig('dataset_grafos_no_dirigidos_1000_Value.png')
---
+++ plot_results_time_3000.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('results_MISP_avg.csv')
+data = data[data.Size == 2000]
+data.columns = data.columns.str.strip()
+
+# print(data)
+
+plt.figure(figsize=(10, 8))
+plt.plot(data['Density'], data['Greedy_Time'], color='r', label='Tiempo medio usando Greedy')
+plt.plot(data['Density'], data['GreedyRan_Time'], color='b', label='Tiempo medio usando RandomGreedy')
+plt.title('Resultados de tiempo entre MISP_Greedy y MISP_RandomGreedy (Tamaño 2000)')
+plt.xlabel('Densidad')
+plt.ylabel('Tiempo (ns)')
+plt.legend()
+plt.tick_params(axis='x')
+plt.tick_params(axis='y')
+plt.grid(True)
+plt.savefig('dataset_grafos_no_dirigidos_2000_Time.png')
