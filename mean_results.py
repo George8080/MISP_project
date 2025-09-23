dfecb8a38ee634fc67a6c1a43df48d7e5bc3e043
George8080 <santisjorge8000@gmail.com>
2025-09-23 18:08:23 -0300

    Obtaining the average of the results

---
+++ mean_results.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+file_path = './results_MISP.csv'
+df = pd.read_csv(file_path)
+
+df_grouped = df.drop(columns='Number').groupby(['Size', 'Density']).mean().reset_index()
+df_grouped.to_csv('results_MISP_avg.csv', index=False)
+
+# print(df_grouped)
